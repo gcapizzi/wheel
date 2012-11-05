724110e8cf9f8e89132281763147f419784affbb
Giuseppe Capizzi <g.capizzi@gmail.com>
2012-11-06 00:06:47 +0100

    Put Account class in Scrooge module

--- model/account.rb
+++ model/account.rb
@@ -1,21 +1,25 @@
-class Account
-  attr :name
-  attr_reader :transactions
+module Scrooge
 
-  def initialize(name = '')
-    @name = name
-    @transactions = []
+  class Account
+    attr :name
+    attr_reader :transactions
+
+    def initialize(name = '')
+      @name = name
+      @transactions = []
+    end
+
+    def empty?
+      @transactions.empty?
+    end
+
+    def add(transaction)
+      @transactions << transaction
+    end
+
+    def last
+      @transactions.last
+    end
   end
 
-  def empty?
-    @transactions.empty?
-  end
-
-  def add(transaction)
-    @transactions << transaction
-  end
-
-  def last
-    @transactions.last
-  end
 end
