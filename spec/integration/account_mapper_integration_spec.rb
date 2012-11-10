d38e8db60a5eed93433fa12bdc5210ce270228b1
Giuseppe Capizzi <g.capizzi@gmail.com>
2012-11-10 16:50:09 +0100

    More integration specs

--- spec/integration/account_mapper_integration_spec.rb
+++ spec/integration/account_mapper_integration_spec.rb
@@ -18,11 +18,33 @@ module Scrooge
       account
     end
 
-    it 'finds a previously inserted account' do
+    it 'finds a previously inserted account by id' do
       mapper.insert(account)
       found = mapper.find(id: account.id)
       found.id.should == account.id
       found.name.should == account.name
+    end
+
+    it 'finds a previously inserted account by name' do
+      mapper.insert(account)
+      found = mapper.find(name: account.name)
+      found.id.should == account.id
+      found.name.should == account.name
+    end
+
+    it 'updates an account successfully' do
+      mapper.insert(account)
+      account.name = "Test account new name"
+      mapper.update(account)
+      found = mapper.find(id: account.id)
+      found.name.should == account.name
+    end
+
+    it 'deletes and account successfully' do
+      mapper.insert(account)
+      mapper.find(id: account.id).should_not be_nil
+      mapper.delete(account)
+      mapper.find(id:account.id).should be_nil
     end
   end
 end
