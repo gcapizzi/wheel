134d4442913234e62bf989d8942e157cfac96e2c
Giuseppe Capizzi <g.capizzi@gmail.com>
2012-11-15 13:39:26 +0100

    Rearrange integration tests

--- spec/integration/account_mapper_integration_spec.rb
+++ spec/integration/account_mapper_integration_spec.rb
@@ -14,33 +14,39 @@ module Scrooge
     let(:mapper) { AccountMapper.new(dataset) }
     let(:account) { Account.new("Test account") }
 
-    it 'finds a previously inserted account by id' do
-      mapper.save(account)
-      found = mapper.find(id: account.id)
-      found.id.should == account.id
-      found.name.should == account.name
+    describe '#save' do
+      it 'updates an account successfully' do
+        mapper.save(account)
+        account.name = "Test account new name"
+        mapper.save(account)
+        found = mapper.find(id: account.id)
+        found.name.should == account.name
+      end
     end
 
-    it 'finds a previously inserted account by name' do
-      mapper.save(account)
-      found = mapper.find(name: account.name)
-      found.id.should == account.id
-      found.name.should == account.name
+    describe '#find' do
+      it 'finds a previously inserted account by id' do
+        mapper.save(account)
+        found = mapper.find(id: account.id)
+        found.id.should == account.id
+        found.name.should == account.name
+      end
+
+      it 'finds a previously inserted account by name' do
+        mapper.save(account)
+        found = mapper.find(name: account.name)
+        found.id.should == account.id
+        found.name.should == account.name
+      end
     end
 
-    it 'updates an account successfully' do
-      mapper.save(account)
-      account.name = "Test account new name"
-      mapper.save(account)
-      found = mapper.find(id: account.id)
-      found.name.should == account.name
-    end
-
-    it 'deletes and account successfully' do
-      mapper.save(account)
-      mapper.find(id: account.id).should_not be_nil
-      mapper.delete(account)
-      mapper.find(id:account.id).should be_nil
+    describe '#delete' do
+      it 'deletes an account successfully' do
+        mapper.save(account)
+        mapper.find(id: account.id).should_not be_nil
+        mapper.delete(account)
+        mapper.find(id:account.id).should be_nil
+      end
     end
   end
 end
