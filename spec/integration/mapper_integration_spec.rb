e303cf7932f5f145b02f952d4fb6fe290ccd8112
Giuseppe Capizzi <g.capizzi@gmail.com>
2012-11-26 23:00:43 +0100

    Mapper integration spec

---
+++ spec/integration/mapper_integration_spec.rb
@@ -0,0 +1,75 @@
+require 'spec_helper'
+require_relative '../../lib/mapper'
+
+module Scrooge
+
+  class Fake < Struct.new(:id, :one, :two, :three); end
+
+  describe Mapper do
+    let(:dataset) do
+      db = Sequel.sqlite
+      db.create_table :fake do
+        primary_key :id
+        String :one
+        Integer :two
+        Decimal :three
+      end
+      db[:fake]
+    end
+    let(:mapping) do
+      class FakeMapping < Mapping; end
+      FakeMapping.maps Fake
+      FakeMapping.fields :one, :two, :three
+      FakeMapping
+    end
+    let(:mapper) { Mapper.new(dataset, mapping) }
+    let(:object) { Fake.new(nil, "one", 2, 3.0) }
+
+    describe '#save' do
+      context 'when the object has never been saved' do
+        it 'calls insert on the dataset and sets the object id' do
+          mapper.save(object)
+          found = mapper.find(object.id)
+          found.one.should == object.one
+          found.two.should == object.two
+          found.three.should == object.three
+        end
+      end
+
+      context 'when the object has already been saved' do
+        it 'calls update on the dataset' do
+          mapper.save(object)
+          object.one = "new one"
+          object.two = 22
+          object.three = 3.33
+          mapper.save(object)
+          found = mapper.find(object.id)
+          found.one.should == object.one
+          found.two.should == object.two
+          found.three.should == object.three
+        end
+      end
+    end
+
+    describe '#find' do
+      it 'finds a previously inserted account' do
+        mapper.save(object)
+        found = mapper.find(object.id)
+        found.id.should == object.id
+        found.one.should == object.one
+        found.two.should == object.two
+        found.three.should == object.three
+      end
+    end
+
+    describe '#delete' do
+      it 'deletes an account successfully' do
+        mapper.save(object)
+        mapper.find(object.id).should_not be_nil
+        mapper.delete(object)
+        mapper.find(object.id).should be_nil
+      end
+    end
+  end
+
+end
