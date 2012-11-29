8ed799ae49e727fa997f1d0ef9ae063a70ad4e64
Giuseppe Capizzi <g.capizzi@gmail.com>
2012-11-29 22:53:54 +0100

    Improve Mapper spec

--- spec/unit/mapper_spec.rb
+++ spec/unit/mapper_spec.rb
@@ -7,42 +7,49 @@ module Scrooge
   describe Mapper do
     let(:db) { double("DB") }
     let(:dataset) { double("Dataset") }
+    let(:object_dataset) { double("Object dataset") }
+    let(:empty_dataset) { double("Empty dataset") }
     let(:mapping) { double("Mapping") }
     let(:mapper) { Mapper.new(db, mapping) }
+    let(:table) { :fake }
 
     let(:attributes) { { a: 1, b: 2, c: 3 } }
-    let(:object) { OpenStruct.new(attributes) }
     let(:saved_attributes) { attributes.merge(id: 1) }
-    let(:saved_object) { OpenStruct.new(saved_attributes) }
+    let(:object) { double("Object") }
 
-    before do
-      db.stub(:[]).and_return(dataset)
-      mapping.stub(:table).and_return(:table)
+    def should_get_table_dataset!
+      mapping.should_receive(:table).and_return(table)
+      db.should_receive(:[]).with(table).and_return(dataset)
+    end
+
+    def should_get_object_dataset!
+      object.should_receive(:id).at_least(:once).and_return(1)
+      dataset.should_receive(:where).with(id: 1).and_return(object_dataset)
     end
 
     describe '#save' do
       context 'when the object has never been saved' do
         it 'calls insert on the dataset and sets the object id' do
+          should_get_table_dataset!
           mapping.should_receive(:to_record).with(object)
             .and_return(attributes)
+          object.should_receive(:id).and_return(nil)
           dataset.should_receive(:insert).with(attributes).and_return(1)
+          object.should_receive(:id=).with(1)
 
           mapper.save(object)
-
-          object.id.should == 1
         end
       end
 
       context 'when the object has already been saved' do
         it 'calls update on the dataset' do
-          mapping.should_receive(:to_record).with(saved_object)
+          should_get_table_dataset!
+          should_get_object_dataset!
+          mapping.should_receive(:to_record).with(object)
             .and_return(attributes)
-          object_dataset = double("Object dataset")
-          dataset.should_receive(:where).with(id: saved_object.id)
-            .and_return(object_dataset)
           object_dataset.should_receive(:update).with(attributes)
 
-          mapper.save(saved_object)
+          mapper.save(object)
         end
       end
     end
@@ -50,40 +57,40 @@ module Scrooge
     describe '#delete' do
       context 'when the object has already been saved' do
         it 'calls delete on the dataset' do
-          object_dataset = double("Object dataset")
-          dataset.should_receive(:where).with(id: saved_object.id)
-            .and_return(object_dataset)
+          should_get_table_dataset!
+          should_get_object_dataset!
           object_dataset.should_receive(:delete)
 
-          mapper.delete(saved_object)
+          mapper.delete(object)
         end
       end
 
       context 'when the object has never been saved' do
-         it 'does nothing' do
-           dataset.should_not_receive(:where)
+        it 'does nothing' do
+          object.should_receive(:id).and_return(nil)
+          dataset.should_not_receive(:where)
 
-           mapper.delete(object)
-         end
+          mapper.delete(object)
+        end
       end
     end
 
     describe '#find' do
       it 'calls where on the dataset' do
-        object_dataset = double("Object dataset")
+        should_get_table_dataset!
         dataset.should_receive(:where).with(id: 1).and_return(object_dataset)
-        object_dataset.stub(:empty?).and_return(false)
+        object_dataset.should_receive(:empty?).and_return(false)
         object_dataset.should_receive(:first).and_return(saved_attributes)
-        mapping.should_receive(:from_record)
-          .with(saved_attributes).and_return(saved_object)
+        mapping.should_receive(:from_record).with(saved_attributes)
+          .and_return(object)
 
         found = mapper.find(1)
 
-        found.should == saved_object
+        found.should == object
       end
 
       it 'returns nil if no object is found' do
-        empty_dataset = double("Empty dataset")
+        should_get_table_dataset!
         dataset.should_receive(:where).with(id: 99).and_return(empty_dataset)
         empty_dataset.should_receive(:empty?).and_return(true)
 
