6ba20018b2a3bdd7f3f6ea466b2963d372177394
Giuseppe Capizzi <g.capizzi@gmail.com>
2014-10-11 18:25:49 +0200

    Specs refactoring

--- spec/integration/mapper_integration_spec.rb
+++ spec/integration/mapper_integration_spec.rb
@@ -7,32 +7,31 @@ module Wheel
   class Fake < Struct.new(:id, :one, :two, :three); end
 
   describe Mapper do
-    let(:db) do
-      db = Sequel.sqlite ''
+    let(:logger) { Logger.new($stdout) }
+    let(:db) { Sequel.sqlite '', :loggers => [logger] }
+    let(:mapping) { Mapping.new(Fake) }
+    let(:mapper) { Mapper.new(db, mapping) }
+    let(:object) { Fake.new(nil, "one", 2, 3.0) }
+
+    before do
+      db.sql_log_level = :debug
       db.create_table :fake do
         primary_key :id
         String :one
         Integer :two
         Decimal :three
       end
-      db
-    end
-    let (:mapping) do
-      mapping = Mapping.new(Fake)
+
       mapping.fields :one, :two, :three
-      mapping
     end
-    let(:mapper) { Mapper.new(db, mapping) }
-    let(:object) { Fake.new(nil, "one", 2, 3.0) }
 
     describe '#save' do
       context 'when the object has never been saved' do
         it 'calls insert on the dataset and sets the object id' do
           mapper.save(object)
           found = mapper.find(object.id)
-          expect(found.one).to eq(object.one)
-          expect(found.two).to eq(object.two)
-          expect(found.three).to eq(object.three)
+
+          expect(found).to eq(object)
         end
       end
 
@@ -44,9 +43,8 @@ module Wheel
           object.three = 3.33
           mapper.save(object)
           found = mapper.find(object.id)
-          expect(found.one).to eq(object.one)
-          expect(found.two).to eq(object.two)
-          expect(found.three).to eq(object.three)
+
+          expect(found).to eq(object)
         end
       end
     end
@@ -55,10 +53,8 @@ module Wheel
       it 'finds a previously inserted account' do
         mapper.save(object)
         found = mapper.find(object.id)
-        expect(found.id).to eq(object.id)
-        expect(found.one).to eq(object.one)
-        expect(found.two).to eq(object.two)
-        expect(found.three).to eq(object.three)
+
+        expect(found).to eq(object)
       end
     end
 
@@ -66,6 +62,7 @@ module Wheel
       it 'deletes an account successfully' do
         mapper.save(object)
         expect(mapper.find(object.id)).not_to be_nil
+
         mapper.delete(object)
         expect(mapper.find(object.id)).to be_nil
       end
